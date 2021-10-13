a36e2239b3b3d05b037cf984e589672266b9cc01
wlx0048 <64272810+wlx0048@users.noreply.github.com>
2021-10-13 23:30:45 +0800

    Delete the-firstgame directory

--- the-firstgame/README.md
+++
@@ -1,4 +0,0 @@
-# the-firstgame
-[百度云盘][https://pan.baidu.com/s/1dwe3_O8UsmSWlXbFU-zl1w ]
-
-提取码：1223
--- the-firstgame/Source/myfirstgame/myfirstgame.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "myfirstgame.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, myfirstgame, "myfirstgame" );
- 
